096ecb5da1110ba6d22a24167f0a9bf010b7d821
Balazs <balazs.galambos@yahoo.com>
2026-04-08 21:30:35 +0200

    added tiles

--- tiles_src/fb-level10-tiles.tsx
+++ tiles_src/fb-level10-tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="fb-level10-tileset" tilewidth="128" tileheight="144" tilecount="49" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="fb-level10-tileset" tilewidth="384" tileheight="288" tilecount="77" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="png/left-edge-1wide_b.png" width="64" height="144"/>
@@ -147,5 +147,89 @@
  </tile>
  <tile id="50">
   <image source="png/left-edge-top-1wide.png" width="64" height="144"/>
+ </tile>
+ <tile id="51">
+  <image source="png/floating-platform-2wide-2high.png" width="128" height="288"/>
+ </tile>
+ <tile id="52">
+  <image source="png/floating-platform-broken-2wide-2heigh.png" width="128" height="288"/>
+ </tile>
+ <tile id="53">
+  <image source="png/floating-wood-platform-3wide-up.png" width="384" height="144"/>
+ </tile>
+ <tile id="54">
+  <image source="png/floating-wood-platform-3wide.png" width="384" height="144"/>
+ </tile>
+ <tile id="55">
+  <image source="png/middle-transition-rock-rustediron-3-wide-b.png" width="384" height="144"/>
+ </tile>
+ <tile id="56">
+  <image source="png/middle-transition-rock-rustediron-3-wide-up-b.png" width="384" height="144"/>
+ </tile>
+ <tile id="57">
+  <image source="png/middle-transition-rock-rustediron-3-wide-up.png" width="384" height="144"/>
+ </tile>
+ <tile id="58">
+  <image source="png/middle-transition-rock-rustediron-3-wide.png" width="384" height="144"/>
+ </tile>
+ <tile id="59">
+  <image source="png/middle-transition-rock-wood-3-wide-b.png" width="384" height="144"/>
+ </tile>
+ <tile id="60">
+  <image source="png/middle-transition-rock-wood-3-wide-up-b.png" width="384" height="144"/>
+ </tile>
+ <tile id="61">
+  <image source="png/middle-transition-rock-wood-3-wide-up.png" width="384" height="144"/>
+ </tile>
+ <tile id="63">
+  <image source="png/water-2wide.png" width="128" height="144"/>
+ </tile>
+ <tile id="64">
+  <image source="png/water-withwall-2wide.png" width="128" height="144"/>
+ </tile>
+ <tile id="65">
+  <image source="png/middle-rusted-2wide_b.png" width="128" height="144"/>
+ </tile>
+ <tile id="66">
+  <image source="png/middle-rusted-2wide-c.png" width="128" height="144"/>
+ </tile>
+ <tile id="67">
+  <image source="png/middle-rusted-2wide.png" width="128" height="144"/>
+ </tile>
+ <tile id="68">
+  <image source="png/middle-rusted-up-2wide_b.png" width="128" height="144"/>
+ </tile>
+ <tile id="69">
+  <image source="png/middle-rusted-up-2wide_c.png" width="128" height="144"/>
+ </tile>
+ <tile id="70">
+  <image source="png/middle-rusted-up-2wide_d.png" width="128" height="144"/>
+ </tile>
+ <tile id="71">
+  <image source="png/middle-rusted-up-2wide.xcf.png" width="128" height="144"/>
+ </tile>
+ <tile id="72">
+  <image source="png/water-2wide-b.png" width="128" height="144"/>
+ </tile>
+ <tile id="73">
+  <image source="png/water-2wide-c.png" width="128" height="144"/>
+ </tile>
+ <tile id="74">
+  <image source="png/water-2wide-d.png" width="128" height="144"/>
+ </tile>
+ <tile id="75">
+  <image source="png/water-2wide-e.png" width="128" height="144"/>
+ </tile>
+ <tile id="76">
+  <image source="png/water-2wide-g.png" width="128" height="144"/>
+ </tile>
+ <tile id="77">
+  <image source="png/water-2wide-f.png" width="128" height="144"/>
+ </tile>
+ <tile id="78">
+  <image source="png/water-2wideb.png" width="128" height="144"/>
+ </tile>
+ <tile id="81">
+  <image source="png/middle-transition-rock-wood-3-wide.png" width="384" height="144"/>
  </tile>
 </tileset>
